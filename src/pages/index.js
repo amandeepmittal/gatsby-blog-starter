f06079df2f85a1db8da267182f7af394cd56175c
Aman Mittal <amandeepmittal@live.com>
2018-11-17 14:41:10 +0530

    🚀

---
+++ src/pages/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { graphql, Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = ({ data }) => {
+  const { edges } = data.allMarkdownRemark
+
+  return (
+    <Layout>
+      <div>
+        {edges.map(edge => {
+          const { frontmatter } = edge.node
+          return (
+            <div key={frontmatter.path}>
+              <Link to={frontmatter.path}>{frontmatter.title}</Link>
+              &nbsp;
+              <small>
+                {' '}
+                <em>published on</em> {frontmatter.date}
+              </small>
+              <p>{frontmatter.excerpt}</p>
+              <br />
+            </div>
+          )
+        })}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query HomePageQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+            tags
+            excerpt
+          }
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
